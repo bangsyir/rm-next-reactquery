a65bdfecef77e7290279a0059bd023be0328461c
bangsyir <bangsyir@gmail.com>
2022-12-31 16:33:59 +0800

    add notfound page

---
+++ components/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="text-2xl font-bold">Page Not Found</div>
+      <div className="text-xl">404</div>
+    </div>
+  );
+}
