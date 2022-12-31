83d22f04d20e090263fb7410e35d89579996706b
bangsyir <bangsyir@gmail.com>
2022-12-31 16:31:16 +0800

    character getter

--- libs/characters.ts
+++ libs/characters.ts
@@ -1,6 +1,11 @@
 import appAxios from "./axios";
 
-export const getCharacters = async (page:number) => {
+export const getCharacters = async (page: number) => {
   const res = await appAxios.get(`/character?page=${page}`);
+  return res.data;
+};
+
+export const getCharacter = async (id: any) => {
+  const res = await appAxios.get(`/character/${id}`);
   return res.data;
 };
