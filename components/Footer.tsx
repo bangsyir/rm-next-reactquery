019e5ae832100258bafcdff095dbb3c83ef98165
bangsyir <bangsyir@gmail.com>
2023-08-03 15:48:00 +0800

    update footer position

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="container mx-auto pb-4 mt-auto">
+    <footer className="container mx-auto py-4 mt-auto">
       <p className="flex flex-wrap items-center">
         © {year}. All rights reserved. Build with ❤️ by&nbsp;
         <a
